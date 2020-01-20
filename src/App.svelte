93a1b499b822941b979d4fa59e4d336ee51c719d
Matthew Graham <mdg149@gmail.com>
2020-01-20 10:35:13 -0500

    move flashcards into own component
    make it easier to swap in an options component

--- src/App.svelte
+++ src/App.svelte
@@ -1,85 +1,12 @@
 <script>
-  import FlashCard from './FlashCard.svelte';
-	import { slide } from 'svelte/transition';
+  import FlashForward from './FlashForward.svelte';
 
 	let name = 'Third Grader';
-	let random_facts = [];
-	let squares = [];
-	for (var x=2; x<12; x++) {
-  	for (var y=2; y<12; y++) {
-			const product = x * y;
-			random_facts.push([Math.random(), x, y, product, 0]);
-			random_facts.push([Math.random(), x, y, product, 1]);
-			random_facts.push([Math.random(), x, y, product, 2]);
-		}
-	}
-	random_facts.sort()
-	let facts = random_facts.map(function(f) {
-	  f.splice(0, 1);
-	  return f;
-	});
-	
-    let prompt_index = 0;
-    let answer_index = -1;
-	
-	function show_next() {
-        answer_index += 1;
-        prompt_index += 1;
-	}
-
-	function handle_show_next(event) {
-		show_next();
-	}
-
-	function handle_keydown(event) {
-		if (String.fromCharCode(event.keyCode) === " ") {
-			show_next();
-		} else {
-	    // answer = String.fromCharCode(event.keyCode);
-		}
-	}
 </script>
 
 <style>
-	p {
-		font-size: 30pt;
-	}
-	button {
-		width: 200px;
-	}
 </style>
 
-<svelte:window on:keypress={handle_keydown}/>
+<h1>Hi {name}!</h1>
 
-<h1>Hello {name}!</h1>
-
-<p>
-    {#if answer_index == -1}
-      <div
-        transition:slide="{{ delay: 500, duration: 500 }}"
-        style="padding: 10px;"
-        >&nbsp;</div>
-    {/if}
-	{#each facts as fact, k}
-	  {#if k === prompt_index || k === answer_index}
-        <div
-            transition:slide="{{ delay: 500, duration: 500 }}"
-            style="padding: 10px;"
-            >
-	  	<FlashCard
-							 i={k + 1}
-							 x={facts[k][0]}
-						   y={facts[k][1]}
-							 z={facts[k][2]}
-							 unknown={facts[k][3]}
-							 show_answer={k === answer_index}
-							 />
-        </div>
-	  {/if}
-	{/each}
-</p>
-<p id=next_button>
-	<button on:click={handle_show_next}>
-	  next fact
-  </button>
-</p>
+<FlashForward upper=4 />
